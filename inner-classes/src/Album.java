675e1c6e723b74a33de8e39f79910309fef9537d
Prateek13727 <prateek.pandey@tracxn.com>
2017-07-10 23:47:21 +0530

    [InnerClasses]

---
+++ inner-classes/src/Album.java
@@ -0,0 +1,70 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by dev on 18/09/15.
+ */
+public class Album {
+    private String name;
+    private String artist;
+    private SongList songList;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songList = new SongList();
+    }
+
+    private class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+
+        }
+
+        public boolean addSong(String title, double duration) {
+            if(findSong(title) == null) {
+                this.songs.add(new Song(title, duration));
+                return true;
+            }
+            return false;
+        }
+
+        public Song findSong(String title) {
+            for(Song checkedSong: this.songs) {
+                if(checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+    }
+
+    public boolean addSong(String title, double duration) {
+       return this.songList.addSong(title,duration);
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber -1;
+        if((index >0) && (index <= this.songList.songs.size())) {
+            playList.add(this.songList.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = this.songList.findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+}
